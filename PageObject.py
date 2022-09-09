d1de09d57ece21a58981d799d24a05716432fe9b
solddire <sayganov.kirya@mail.ru>
2022-09-09 19:55:37 +0300

    second commit

--- PageObject.py
+++ PageObject.py
@@ -5,28 +5,34 @@ from selenium.webdriver.common.by import By
 # Создаем класс FireFoxLocators. Он будет только для хранения локаторов.
 # В классе описываем локаторы:
 class FireFoxLocators:
-    LOCATOR_COOKIE_BUTTON = (By.ID, "rcc-confirm-button")
-    LOCATOR_TOP_ORDER_BUTTON = (By.CLASS_NAME, "Button_Button__ra12g")
-    LOCATOR_DOWN_ORDER_BUTTON = (By.XPATH, "//button[@class='Button_Button__ra12g Button_UltraBig__UU3Lp']")
+    LOCATOR_COOKIE_BUTTON = (By.ID, "rcc-confirm-button")  # Кнопка Cookie
+    LOCATOR_TOP_ORDER_BUTTON = (By.CLASS_NAME, "Button_Button__ra12g")  # Кнопка заказа(верхняя)
+    LOCATOR_DOWN_ORDER_BUTTON = (
+        By.XPATH, "//button[@class='Button_Button__ra12g Button_UltraBig__UU3Lp']")  # Кнопка заказа(нижняя)
     LOCATOR_NAME_FORM = (By.CSS_SELECTOR, "div.App_App__15LM- div.Order_Content__bmtHS div.Order_Form__17u6u div."
                                           "Input_InputContainer__3NykH:nth-child(1) > input.Input_Input__1iN_Z."
-                                          "Input_Responsible__1jDKN")
+                                          "Input_Responsible__1jDKN")  # Поле для ввода имени
     LOCATOR_LASTNAME_FORM = (By.CSS_SELECTOR, "div.App_App__15LM- div.Order_Content__bmtHS div.Order_Form__17u6u"
                                               " div.Input_InputContainer__3NykH:nth-child(2) > "
-                                              "input.Input_Input__1iN_Z.Input_Responsible__1jDKN")
+                                              "input.Input_Input__1iN_Z.Input_Responsible__1jDKN")  # Поле для ввода
+    # фамилии
     LOCATOR_ADDRESS_FORM = (By.CSS_SELECTOR, "div.App_App__15LM- div.Order_Content__bmtHS div.Order_Form__17u6u "
                                              "div.Input_InputContainer__3NykH:nth-child(3) > "
-                                             "input.Input_Input__1iN_Z.Input_Responsible__1jDKN")
-    LOCATOR_METRO_FORM_BUTTON = (By.CLASS_NAME, "select-search__input")
-    LOCATOR_METRO_FORM_CHOICE = (By.XPATH, "//input[@class='select-search__input']")
+                                             "input.Input_Input__1iN_Z.Input_Responsible__1jDKN")  # Поле для ввода
+    # адреса
+    LOCATOR_METRO_FORM_BUTTON = (By.CLASS_NAME, "select-search__input")  # Кнопка указания метро
+    LOCATOR_METRO_SQUARE_BUTTON = (
+        By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[4]/div/div[2]/ul/li[3]/button")  # Выбор метро
     LOCATOR_PHONE_FORM = (By.CSS_SELECTOR, "div.App_App__15LM- div.Order_Content__bmtHS div.Order_Form__17u6u "
                                            "div.Input_InputContainer__3NykH:nth-child(5) > "
-                                           "input.Input_Input__1iN_Z.Input_Responsible__1jDKN")
+                                           "input.Input_Input__1iN_Z.Input_Responsible__1jDKN")  # Кнопка для ввода
+    # номера телефона
     LOCATOR_CONTINUE_BUTTON = (By.CSS_SELECTOR, "div:nth-child(2) div.App_App__15LM- div.Order_Content__bmtHS "
                                                 "div.Order_NextButton__1_rCA > "
-                                                "button.Button_Button__ra12g.Button_Middle__1CSJM")
-    LOCATOR_METRO_SQUARE_BUTTON = (By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[4]/div/div[2]/ul/li[3]/button")
-    LOCATOR_DATE_BRING_SCOOTER = (By.XPATH, "//div[@class='react-datepicker__input-container']//input[@type='text']")
+                                                "button.Button_Button__ra12g.Button_Middle__1CSJM")  # Кнопка
+    # продолжить(форма заказа)
+    LOCATOR_DATE_BRING_SCOOTER = (
+        By.XPATH, "//div[@class='react-datepicker__input-container']//input[@type='text']")  # Поле для ввода даты
     LOCATOR_DATE_BRING_SCOOTER_BUTTON = (By.CSS_SELECTOR, "div.App_App__15LM- div.Order_Content__bmtHS "
                                                           "div.Order_Form__17u6u div.Order_MixedDatePicker__3qiay "
                                                           "div.react-datepicker__tab-loop div.react-datepicker-popper "
@@ -35,23 +41,29 @@ class FireFoxLocators:
                                                           "div.react-datepicker__month "
                                                           "div.react-datepicker__week:nth-child(3) > "
                                                           "div.react-datepicker__day.react-datepicker__day--018.react"
-                                                          "-datepicker__day--weekend:nth-child(7)")
-    LOCATOR_RENTAL_PERIOD_BUTTON = (By.XPATH, "//div[@class='Dropdown-control']")
-    LOCATOR_RENTAL_PERIOD_CHOICE = (By.CSS_SELECTOR, "div.Dropdown-option:nth-child(1)")
-    LOCATOR_COLOR_SCOOTER = (By.CLASS_NAME, "Checkbox_Input__14A2w")
+                                                          "-datepicker__day--weekend:nth-child(7)")  # Подтверждение
+    # даты в datepicker
+    LOCATOR_RENTAL_PERIOD_BUTTON = (
+        By.XPATH, "//div[@class='Dropdown-control']")  # Кнопка выбора периода времени катания
+    LOCATOR_RENTAL_PERIOD_CHOICE = (By.CSS_SELECTOR, "div.Dropdown-option:nth-child(1)")  # Выбираем период времени
+    LOCATOR_COLOR_SCOOTER = (By.CLASS_NAME, "Checkbox_Input__14A2w")  # Выбираем цвет самоката
     LOCATOR_COMMENTARY = (By.XPATH, "//div[@class='Order_Form__17u6u']//div["
-                                    "@class='Input_InputContainer__3NykH']//input[@type='text']")
+                                    "@class='Input_InputContainer__3NykH']//input[@type='text']")  # Поле для ввода
+    # комментария
     LOCATOR_ORDER_BUTTON = (By.CSS_SELECTOR, "div:nth-child(2) div.App_App__15LM- div.Order_Content__bmtHS "
                                              "div.Order_Buttons__1xGrp > "
-                                             "button.Button_Button__ra12g.Button_Middle__1CSJM:nth-child(2)")
+                                             "button.Button_Button__ra12g.Button_Middle__1CSJM:nth-child(2)")  #
+    # Кнопка заказать в форме "заказа"
     LOCATOR_CONFIRM_BUTTON = (By.CSS_SELECTOR, "div.App_App__15LM- div.Order_Content__bmtHS div.Order_Modal__YZ-d3 "
                                                "div.Order_Buttons__1xGrp > "
-                                               "button.Button_Button__ra12g.Button_Middle__1CSJM:nth-child(2)")
+                                               "button.Button_Button__ra12g.Button_Middle__1CSJM:nth-child(2)")  #
+    # Кнопка подтверждения заказа
     LOCATOR_CHECK_STATUS = (By.CSS_SELECTOR, "div.App_App__15LM- div.Order_Content__bmtHS div.Order_Modal__YZ-d3 "
                                              "div.Order_NextButton__1_rCA > "
-                                             "button.Button_Button__ra12g.Button_Middle__1CSJM")
-    LOCATOR_YANDEX_BUTTON = (By.CLASS_NAME, "Header_LogoYandex__3TSOI")
-    LOCATOR_SCOOTER_BUTTON = (By.CLASS_NAME, "Header_LogoScooter__3lsAR")
+                                             "button.Button_Button__ra12g.Button_Middle__1CSJM")  # Кнопка проверить
+    # статус
+    LOCATOR_YANDEX_BUTTON = (By.CLASS_NAME, "Header_LogoYandex__3TSOI")  # Кнопка "Яндекс"
+    LOCATOR_SCOOTER_BUTTON = (By.CLASS_NAME, "Header_LogoScooter__3lsAR")  # Кнопка "Самокаты"
 
 
 # Создаем класс Question_Program, наследуемся от BasePage.
@@ -63,45 +75,57 @@ class Main_Page(BasePage):
     def click_to_order_down(self):
         self.driverwait(FireFoxLocators.LOCATOR_DOWN_ORDER_BUTTON).click()
 
-    # В данных методах реализуем нажатие на кнопки.
+    # В данных методах реализуем нажатие на кнопки. В click_to_order_top нажимается верхняя кнопка и кнопка Cookie.
+    # click_to_order_down - В этом методе нажатие на нижнюю кнопку заказа.
     # P.S. В первом методе больше кликов, потому что кнопка куки мешает для нажатия.
 
 
 class Form_Order(BasePage):
+    # В fill_name заполняем имя и фамилию.
     def fill_name(self):
         self.driverwait(FireFoxLocators.LOCATOR_NAME_FORM).send_keys('Алексей')
         self.driverwait(FireFoxLocators.LOCATOR_LASTNAME_FORM).send_keys('Великолепный')
 
+    # В fill_address заполняем адрес и метро.
     def fill_address(self):
         self.driverwait(FireFoxLocators.LOCATOR_ADDRESS_FORM).send_keys('Улица Маршала Бирюзова, дом 42')
         self.driverwait(FireFoxLocators.LOCATOR_METRO_FORM_BUTTON).click()
         self.driverwait(FireFoxLocators.LOCATOR_METRO_SQUARE_BUTTON).click()
 
+    # В fill_phone заполняем номер телефона и нажимаем далее.
     def fill_phone(self):
         self.driverwait(FireFoxLocators.LOCATOR_PHONE_FORM).send_keys('89612653265')
         self.driverwait(FireFoxLocators.LOCATOR_CONTINUE_BUTTON).click()
 
 
+# Класс About_Rent создан для окна "Про Аренду".
 class About_Rent(BasePage):
+    # В fill_date заполняем дату.
     def fill_date(self):
         self.driverwait(FireFoxLocators.LOCATOR_DATE_BRING_SCOOTER).send_keys('18.09.2022')
         self.driverwait(FireFoxLocators.LOCATOR_DATE_BRING_SCOOTER_BUTTON).click()
         self.driverwait(FireFoxLocators.LOCATOR_RENTAL_PERIOD_BUTTON).click()
         self.driverwait(FireFoxLocators.LOCATOR_RENTAL_PERIOD_CHOICE).click()
 
+    # В fill_other заполняем цвет, комментарий, и нажимаем кнопку заказать, подтверждаем заказ.
     def fill_other(self):
         self.driverwait(FireFoxLocators.LOCATOR_COLOR_SCOOTER).click()
         self.driverwait(FireFoxLocators.LOCATOR_COMMENTARY).send_keys('Привести к 18:00.')
         self.driverwait(FireFoxLocators.LOCATOR_ORDER_BUTTON).click()
         self.driverwait(FireFoxLocators.LOCATOR_CONFIRM_BUTTON).click()
 
+    # В check_status нажимаем на кнопку "проверить статус". Вынес отдельно,чтобы проверка в Tests(
+    # test_check_exe_of_the_order) состоялась.
     def check_status(self):
         self.driverwait(FireFoxLocators.LOCATOR_CHECK_STATUS).click()
 
 
+# Класс YandexButton создан для кнопок "Яндекс" и "Самокаты".
 class YandexButton(BasePage):
+    # В click_on_yandex_button мы нажимаем на кнопку "Яндекс".
     def click_on_yandex_button(self):
         self.driverwait(FireFoxLocators.LOCATOR_YANDEX_BUTTON).click()
 
+    # В click_on_scooter_button мы нажимаем на кнопку "Самокаты".
     def click_on_scooter_button(self):
         self.driverwait(FireFoxLocators.LOCATOR_SCOOTER_BUTTON).click()
--- Tests.py
+++ Tests.py
@@ -10,94 +10,103 @@ class TestProgram:
     @allure.feature('Test button')
     @allure.story('Проверяем кнопку заказать')
     def test_click_button_top(self, browser):
-        main = Main_Page(browser)  # Создаем экземпляр класса first_question
+        main = Main_Page(browser)  # Создаем экземпляр класса main.
         main.go_to_site()  # Запускаем сайт, это делается только один раз!
-        main.click_to_order_top()
+        main.click_to_order_top()  # Нажимаем на кнопку заказать.
 
     @allure.title('Заполнение имени')
     @allure.feature('Filling out name')
     @allure.story('Заполняем имя и фамилию')
     def test_fill_out_name(self, browser):
-        write_in_name = Form_Order(browser)
-        write_in_name.fill_name()
+        write_in_name = Form_Order(browser)  # Создаем экземпляр класса write_in_name.
+        write_in_name.fill_name()  # Заполняем данным методом имя и фамилию.
 
     @allure.title('Заполнение адреса')
     @allure.feature('Filling out address')
     @allure.story('Заполняем адрес')
     def test_fill_out_address(self, browser):
-        write_in_address = Form_Order(browser)
-        write_in_address.fill_address()
+        write_in_address = Form_Order(browser)  # Создаем экземпляр класса write_in_address.
+        write_in_address.fill_address()  # Заполняем данным методом адрес и метро.
 
     @allure.title('Заполнение телефона')
     @allure.feature('Filling out phone number')
     @allure.story('Заполняем номер телефона')
     def test_fill_out_phone(self, browser):
-        write_in_phone = Form_Order(browser)
-        write_in_phone.fill_phone()
+        write_in_phone = Form_Order(browser)  # Создаем экземпляр класса write_in_phone.
+        write_in_phone.fill_phone()  # Заполняем данным методом телефон, нажимаем на кнопку.
 
     @allure.title('Проверка заполнения данных')
     @allure.feature('Checking the filling of data')
     @allure.story('Проверяем, что заполнили данные')
     def test_check_order(self, browser):
-        test = browser.find_element(By.CLASS_NAME, "Order_Header__BZXOb")
+        test = browser.find_element(By.CLASS_NAME,
+                                    "Order_Header__BZXOb")  # В данном методе проверяем, что мы перешли в следующее
+        # окно (Про Аренду).
         assert test.text == "Про аренду"
 
     @allure.title('Заполнение "про аренду"')
     @allure.feature('Fill "about rent"')
     @allure.story('Заполняем "про аренду"')
     def test_fill_rent(self, browser):
-        write_in_rent = About_Rent(browser)
-        write_in_rent.fill_date()
-        write_in_rent.fill_other()
+        write_in_rent = About_Rent(browser)  # Создаем экземпляр класса write_in_rent.
+        write_in_rent.fill_date()  # Заполняем данным методом дату.
+        write_in_rent.fill_other()  # Заполняем цвет, комментарий, и нажимаем кнопку заказать, подтверждаем заказ.
 
     @allure.title('Проверка оформления заказа(верхняя кнопка)')
     @allure.feature('Fill "for rent"(top button)')
     @allure.story('Проверяем оформление заказа(верхняя кнопка)')
     def test_check_exe_of_the_order(self, browser):
-        check = About_Rent(browser)
+        check = About_Rent(browser)  # Создаем экземпляр класса check.
         test = browser.find_element(By.CSS_SELECTOR, "div.App_App__15LM- div.Order_Content__bmtHS "
                                                      "div.Order_Modal__YZ-d3 div.Order_NextButton__1_rCA > "
-                                                     "button.Button_Button__ra12g.Button_Middle__1CSJM")
-        assert test.text == "Посмотреть статус"
-        check.check_status()
+                                                     "button.Button_Button__ra12g.Button_Middle__1CSJM")  # Ищу
+        # кнопку, так как текст динамичен, и номер заказа не предугадать.
+        assert test.text == "Посмотреть статус"  # Проверяем по тексту с кнопки.
+        check.check_status()  # Нажимаем на кнопку "Проверить статус".
 
     @allure.title('Проверка кнопок Яндекса')
     @allure.feature('Check Yandex buttons')
     @allure.story('Проверяем кнопки Яндекса"')
     def test_yandex_buttons(self, browser):
-        click = YandexButton(browser)
-        click.click_on_scooter_button()
-        click.click_on_yandex_button()
-        browser.switch_to_window(browser.window_handles[1])
-        browser.close()
-        browser.switch_to_window(browser.window_handles[0])
+        click = YandexButton(browser)  # Создаем экземпляр класса click.
+        click.click_on_yandex_button()  # Нажимаем на кнопку "Яндекс".
+        browser.switch_to_window(browser.window_handles[1])  # Переключаемся на страницу "Яндекса".
+        url = browser.current_url  # Берем текущий url, чтобы сверить, верно ли все произошло.
+        assert url == 'https://yandex.ru/'  # Сравниваем url'ы.
+        browser.close()  # Закрываем окно "Яндекс".
+        browser.switch_to_window(browser.window_handles[0])  # Переходим обратно в "основное" окно.
+        url = browser.current_url  # Берем текущий url, чтобы сверить, верно ли все произошло.
+        click.click_on_scooter_button()  # Нажимаем на кнопку "Самокаты".
+        assert url == 'https://qa-scooter.praktikum-services.ru/'  # Сравниваем url'ы.
 
     @allure.title('Проверка кнопки заказа(нижняя)')
     @allure.feature('Check button(down)')
     @allure.story('Проверяем кнопку заказ(нижняя)"')
     def test_order_form_down(self, browser):
-        order_form = Main_Page(browser)
-        element = browser.find_element(By.XPATH, "//button[@class='Button_Button__ra12g Button_UltraBig__UU3Lp']")
+        order_form = Main_Page(browser)  # Создаем экземпляр класса order_form.
+        element = browser.find_element(By.XPATH, "//button[@class='Button_Button__ra12g Button_UltraBig__UU3Lp']")  # Кнопка "Заказать" нижняя.
         browser.execute_script("arguments[0].scrollIntoView();", element)  # Здесь пришлось делать скрипт, для того
-        # чтобы страница скроллилась вниз, иначе все это не работает!!
-        order_form.click_to_order_down()
-        fill_form = Form_Order(browser)
-        fill_form.fill_name()
-        fill_form.fill_address()
-        fill_form.fill_phone()
-        test = browser.find_element(By.CLASS_NAME, "Order_Header__BZXOb")
+        # чтобы страница скроллилась вниз, до нашего элемента!
+        order_form.click_to_order_down()  # Нажимаем на кнопку заказать.
+        fill_form = Form_Order(browser)  # Создаем экземпляр класса fill_form
+        fill_form.fill_name()  # Заполняем данным методом имя и фамилию.
+        fill_form.fill_address()  # Заполняем данным методом адрес и метро.
+        fill_form.fill_phone()  # Заполняем данным методом телефон, нажимаем на кнопку.
+        test = browser.find_element(By.CLASS_NAME, "Order_Header__BZXOb") # Проверяем, что мы перешли в следующее
+        # окно (Про Аренду).
         assert test.text == "Про аренду"
 
     @allure.title('Проверка оформления заказа(нижняя кнопка) ')
     @allure.feature('Fill "for rent"(down)')
     @allure.story('Проверяем оформление заказа(нижняя кнопка)"')
     def test_about_rent_down(self, browser):
-        write_in_rent = About_Rent(browser)
-        write_in_rent.fill_date()
-        write_in_rent.fill_other()
-        check = About_Rent(browser)
+        write_in_rent = About_Rent(browser)  # Создаем экземпляр класса write_in_rent.
+        write_in_rent.fill_date()  # Заполняем данным методом дату.
+        write_in_rent.fill_other()  # Заполняем цвет, комментарий, и нажимаем кнопку заказать, подтверждаем заказ.
+        check = About_Rent(browser)  # Создаем экземпляр класса check.
         test = browser.find_element(By.CSS_SELECTOR, "div.App_App__15LM- div.Order_Content__bmtHS "
                                                      "div.Order_Modal__YZ-d3 div.Order_NextButton__1_rCA > "
-                                                     "button.Button_Button__ra12g.Button_Middle__1CSJM")
-        assert test.text == "Посмотреть статус"
-        check.check_status()
+                                                     "button.Button_Button__ra12g.Button_Middle__1CSJM")# Ищу
+        # кнопку, так как текст динамичен, и номер заказа не предугадать.
+        assert test.text == "Посмотреть статус"  # Проверяем по тексту с кнопки.
+        check.check_status()  # Нажимаем на кнопку "Проверить статус".
